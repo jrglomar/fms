463c483878c3b80195d5169fef2a850f4b232610
YoojinKazama <57902036+YoojinKazama@users.noreply.github.com>
2022-06-28 01:52:45 +0800

    last minute changes

--- resources/views/acad_head/activity_view/activity_view_content.blade.php
+++ resources/views/acad_head/activity_view/activity_view_content.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-8 col-sm-8 col-lg-8">
-        <div class="card-primary">
+        <div class="card card-primary">
             <div class="card-header">
                 <div class="align-items-start">
                         <h3 class="text-primary card-title"><i class="fa fa-briefcase" aria-hidden="true"></i> <span id="title"></span></h3>
@@ -20,7 +20,7 @@
         </div>
     </div>
     <div class="col-4 col-sm-4 col-lg-4">
-        <div class="card-primary">
+        <div class="card card-primary">
             <div class="card-header">
                 <div class="align-items-start">
                     <h4 class="text-primary card-title"><i class="fa fa-info-circle"></i> Activity Details</h4>
@@ -61,9 +61,9 @@
 </div>
 <div class="row">
     <div class="col-md-12">
-        <div class="card collapse" id="memo_card">
+        <div class="card card-primary collapse" id="memo_card">
             <div class="card-header">
-                <h2 class="text-primary">Memorandum</h2>
+                <h4 class="text-primary">Memorandum</h4>
             </div>
             <div class="card-body" id="if_memo">
                 
