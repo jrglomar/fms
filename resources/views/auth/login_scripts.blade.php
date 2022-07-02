e5db6e1f2304f7127be873a272e710f326e1846c
John Raven <jrglomar016@gmail.com>
2022-07-02 09:44:21 +0800

    added loader, success function and error message on login

--- resources/views/auth/login_scripts.blade.php
+++ resources/views/auth/login_scripts.blade.php
@@ -7,6 +7,40 @@
         var API_TOKEN = localStorage.getItem("API_TOKEN")
         // END OF GLOBAL VARIABLE
 
+        function checkLoggedIn(){
+            if(localStorage.getItem("USER_DATA") != null){
+                let USER_DATA = localStorage.getItem("USER_DATA")
+                let new_user_data = (JSON.parse(USER_DATA))
+                console.log(new_user_data)
+
+                let role = []
+
+                $.each(new_user_data.user_role, function(i){
+                    role.push(new_user_data.user_role[i].role.title)
+                })
+            
+                if(role.includes('Admin')){
+                window.location.href = "/admin/dashboard"
+                }
+                else if(role.includes('Academic Head')){
+                    window.location.href = "/acad_head/dashboard"
+                }
+                else if(role.includes('Faculty')){
+                    window.location.href = "/faculty/dashboard"
+                }
+                else if(role.includes('Checker')){
+                    window.location.href = "/checker/dashboard"
+                }
+                else{
+                    window.location.href = "/"
+                }
+            }
+            else{
+                removeLoader()
+            }
+        }
+        
+        checkLoggedIn()
 
         $('#loginForm').on('submit', function(e){
             e.preventDefault()
@@ -41,24 +75,27 @@
                         role.push(data.user.user_role[i].role.title)
                     })
                     
-                    if(role.includes('Admin')){
+                    toastr.success('Login Success')
+                    setInterval(function(){
+                        if(role.includes('Admin')){
                         window.location.href = "/admin/dashboard"
-                    }
-                    else if(role.includes('Academic Head')){
-                        window.location.href = "/acad_head/dashboard"
-                    }
-                    else if(role.includes('Faculty')){
-                        window.location.href = "/faculty/dashboard"
-                    }
-                    else if(role.includes('Checker')){
-                        window.location.href = "/checker/dashboard"
-                    }
-                    else{
-                        window.location.href = "/"
-                    }
-
+                        }
+                        else if(role.includes('Academic Head')){
+                            window.location.href = "/acad_head/dashboard"
+                        }
+                        else if(role.includes('Faculty')){
+                            window.location.href = "/faculty/dashboard"
+                        }
+                        else if(role.includes('Checker')){
+                            window.location.href = "/checker/dashboard"
+                        }
+                        else{
+                            window.location.href = "/"
+                        }
+                    }, 2000)
                 },
                 error: function(error){
+                    swalAlert('warning', `${error.responseJSON.message}`)
                     console.log(error)
                     console.log(`message: ${error.responseJSON.message}`)
                     console.log(`status: ${error.status}`)
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,6 +73,7 @@
                 @yield('footer')
         </div>
 
+
         <!-- General JS Scripts -->
         <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"
@@ -101,55 +102,8 @@
 
         <script src="{{ mix('js/app.js') }}"></script>
 
-        <script>
-            function removeLoader(){
-                    $("#loading_cover").fadeOut();
-            };
-
-            function swalAlert(icon, text){
-                Swal.fire({
-                    icon: icon,
-                    text: text
-                })
-            }
-
-            $(document).ready(function(){
-                // GLOBAL VARIABLE
-                var APP_URL = {!! json_encode(url('/')) !!}
-                var API_TOKEN = localStorage.getItem("API_TOKEN")
-                var USER_DATA = localStorage.getItem("USER_DATA")
-                let new_user_data = (JSON.parse(USER_DATA))
-                console.log(new_user_data)
-                let middle_name = ''
-                let user_role = ''
-
-                if(new_user_data.faculty == null){
-                    $('#userNameSidebar').html('Not set')
-                    $('#userRoleSidebar').html('Not set')
-                    $('#userNameNavbar').html('Not set')
-                }
-                else{
-                    $('#sidebar_icon').attr("src", APP_URL + "/" + new_user_data.faculty.image)
-                    if(new_user_data.faculty.middle_name == null){
-                        middle_name = ''
-                    }
-
-                    $('#userNameSidebar').html(new_user_data.faculty.first_name + ' ' + middle_name + ' ' + new_user_data.faculty.last_name)
-                    $.each(new_user_data.user_role, function(i){
-                        if(i < (new_user_data.user_role.length) - 1){
-                            user_role += new_user_data.user_role[i].role.title + ', '
-                        }
-                        else{
-                            user_role += new_user_data.user_role[i].role.title
-                        }
-                    })
-                    $('#userRoleSidebar').html(user_role)
-                    $('#userNameNavbar').html(new_user_data.faculty.first_name)
-                }
-            })
-        </script>
+        @include('layouts/global_custom_scripts')
         
-
         <!-- Scripts -->
         @yield('script')
     </body>
