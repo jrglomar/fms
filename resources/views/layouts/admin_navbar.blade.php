af161bd1616bacb837eb724885ce394a1a2ca533
John Raven <jrglomar016@gmail.com>
2022-07-20 12:36:40 +0800

    added url links on navbar buttons

--- resources/views/layouts/admin_navbar.blade.php
+++ resources/views/layouts/admin_navbar.blade.php
@@ -154,10 +154,10 @@
                             <a id="admin_profile_navbar" href="" class="dropdown-item has-icon">
                                 <i class="far fa-user"></i> Profile
                             </a>
-                            <a href="features-activities.html" class="dropdown-item has-icon">
+                            <a href="features-activities.html" class="dropdown-item has-icon" hidden>
                                 <i class="fas fa-bolt"></i> Activities
                             </a>
-                            <a href="features-settings.html" class="dropdown-item has-icon">
+                            <a href="features-settings.html" class="dropdown-item has-icon" hidden>
                                 <i class="fas fa-cog"></i> Settings
                             </a>
                             <div class="dropdown-divider"></div>
