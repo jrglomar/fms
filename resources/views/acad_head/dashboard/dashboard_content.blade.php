5cc052860e4253825f9bdc232435ed0ccbba6fe1
YoojinKazama <57902036+YoojinKazama@users.noreply.github.com>
2022-07-23 20:49:20 +0800

    added some dashboard again

--- resources/views/acad_head/dashboard/dashboard_content.blade.php
+++ resources/views/acad_head/dashboard/dashboard_content.blade.php
@@ -8,6 +8,7 @@
 </style>
 
       <div class="row">
+
         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
           <div class="card card-statistic-1">
             <div class="card-icon bg-primary">
@@ -23,6 +24,7 @@
             </div>
           </div>
         </div>
+
         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
           <div class="card card-statistic-1">
             <div class="card-icon bg-danger">
@@ -38,6 +40,7 @@
             </div>
           </div>
         </div>
+
         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
           <div class="card card-statistic-1">
             <div class="card-icon bg-warning">
@@ -54,42 +57,41 @@
           </div>
         </div>                 
       </div>
+
       <div class="row">
         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
           <div class="card card-statistic-1">
-            <canvas id="status_donut" height="200" style="min-height: 250px; height: 300px; max-height: 250px; max-width: 100%;"></canvas>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-          <div class="card card-statistic-1">
-            <div class="card-icon bg-danger">
-              <i class="far fa-user"></i>
+            <div class="card-header">
+              <label>Submitted Requirements Status</label>
             </div>
-            <div class="card-wrap">
-              <div class="card-header">
-                <h4>Total Meetings</h4>
-              </div>
-              <div class="card-body" id="total_meeting">
-                
-              </div>
+            <div>
+              <canvas id="rsb_donut"></canvas>
             </div>
           </div>
         </div>
+
         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
           <div class="card card-statistic-1">
-            <div class="card-icon bg-warning">
-              <i class="far fa-user"></i>
+            <div class="card-header">
+              <label>Meeting Status</label>
             </div>
-            <div class="card-wrap">
-              <div class="card-header">
-                <h4>Total Activities</h4>
-              </div>
-              <div class="card-body" id="total_activity">
-                
-              </div>
+            <div>
+              <canvas id="meeting_donut"></canvas>
             </div>
           </div>
-        </div>                 
+        </div>
+
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+          <div class="card card-statistic-1">
+            <div class="card-header">
+              <label>Activity Status</label>
+            </div>
+            <div>
+              <canvas id="activity_donut"></canvas>
+            </div>
+          </div>
+        </div>  
+
       </div>
       
 
--- resources/views/acad_head/dashboard/dashboard_scripts.blade.php
+++ resources/views/acad_head/dashboard/dashboard_scripts.blade.php
@@ -1,4 +1,5 @@
 
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
     $(document).ready(function(){
 
@@ -14,13 +15,54 @@
               dataType: "JSON",
     
               success: function(data){
-                //console.log(data)
+                console.log(data)
                 count = data.length
                 activity_total = 0
+                pend = 0
+                ongoing = 0
+                ended = 0
+
+                date = moment(new Date()).format() 
 
                 for (let i=0; i< count; i++){
                     activity_total += 1
+
+                    if( moment(data[i].start_datetime).format() < date && moment(data[i].end_datetime).format() > date){
+                        ongoing += 1
+                    }
+                    else if(date > moment(data[i].end_datetime).format()){
+                        ended += 1
+                    }
+                    else{
+                        pend += 1
+                    }
                 }
+
+                var donutChartCanvas = $('#activity_donut').get(0).getContext('2d')
+                var donutData        = {
+                  labels: [
+                      'Ongoing',
+                      'Ended',
+                      'Pending',
+                  ],
+                  datasets: [
+                    {
+                      data: [ongoing,ended,pend],
+                      backgroundColor : ['#00a65a', '#f56954', '#f39c12'],
+                    }
+                  ]
+                }
+                var donutOptions     = {
+                  maintainAspectRatio : false,
+                  responsive : true,
+                }
+                //Create pie or douhnut chart
+                // You can switch between pie and douhnut using the method below.
+                new Chart(donutChartCanvas, {
+                  type: 'doughnut',
+                  data: donutData,
+                  options: donutOptions
+                })
 
                 $('#total_activity').html(activity_total)
 
@@ -36,10 +78,48 @@
                 //console.log(data)
                 count = data.length
                 meeting_total = 0
+                ongoing = 0
+                pending = 0
+                done = 0
 
                 for (let i=0; i< count; i++){
                     meeting_total += 1
+                    if( data[i].status == "On Going"){
+                        ongoing += 1
+                    }
+                    else if( data[i].status == "Pending"){
+                        pending += 1
+                    }
+                    else if( data[i].status == "Done"){
+                        done += 1
+                    }
                 }
+
+                var donutChartCanvas = $('#meeting_donut').get(0).getContext('2d')
+                var donutData        = {
+                  labels: [
+                      'Ongoing',
+                      'Pending',
+                      'Done',
+                  ],
+                  datasets: [
+                    {
+                      data: [ongoing,pending,done],
+                      backgroundColor : ['#00a65a', '#f56954', '#f39c12'],
+                    }
+                  ]
+                }
+                var donutOptions     = {
+                  maintainAspectRatio : false,
+                  responsive : true,
+                }
+                //Create pie or douhnut chart
+                // You can switch between pie and douhnut using the method below.
+                new Chart(donutChartCanvas, {
+                  type: 'doughnut',
+                  data: donutData,
+                  options: donutOptions
+                })
 
                 $('#total_meeting').html(meeting_total)
 
@@ -47,42 +127,53 @@
         })
 
         $.ajax({
-              url: APP_URL + '/api/v1/submitted_requirement',
+              url: APP_URL + '/api/v1/requirement_required_faculty_list',
               type: "GET",
               dataType: "JSON",
     
               success: function(data){
-                //console.log(data)
+                console.log(data)
                 count = data.length
                 srb_total = 0
+                app = 0
+                dec = 0
+                rev = 0
 
                 for (let i=0; i< count; i++){
                     srb_total += 1
+
+                    if( data[i].status == "Approved"){
+                        app += 1
+                    }
+                    else if( data[i].status == "Declined"){
+                        dec += 1
+                    }
+                    else if( data[i].status == "Revision"){
+                        rev += 1
+                    }
                 }
 
-                // var donutChartCanvas = $('#status_donut').get(0).getContext('2d')
-                // var donutData        = {
-                //   labels: [
-                //       'Available',
-                //       'Repair',
-                //       'Broken',
-                //       'Missing',
-                //       'Sold',
-                //       'Disposed',
-                //   ],
-                //   datasets: [
-                //     {
-                //       data: [avail,rep,brok,miss,sold,disp],
-                //       backgroundColor : ['#00a65a', '#f56954', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
-                //     }
-                //   ]
-                // }
-                // var donutOptions     = {
-                //   maintainAspectRatio : false,
-                //   responsive : true,
-                // }
-                // //Create pie or douhnut chart
-                // // You can switch between pie and douhnut using the method below.
+                var canvas = document.getElementById("rsb_donut")
+                var donutChartCanvas = $('#rsb_donut').get(0).getContext('2d')
+                var donutData        = {
+                  labels: [
+                      'Approved',
+                      'Decline',
+                      'For Revision',
+                  ],
+                  datasets: [
+                    {
+                      data: [app,dec,rev],
+                      backgroundColor : ['#00a65a', '#f56954', '#f39c12'],
+                    }
+                  ]
+                }
+                var donutOptions     = {
+                  maintainAspectRatio : false,
+                  responsive : true,
+                }
+                //Create pie or douhnut chart
+                // You can switch between pie and douhnut using the method below.
                 new Chart(donutChartCanvas, {
                   type: 'doughnut',
                   data: donutData,
@@ -93,6 +184,7 @@
 
               }
         })
+        
 
 
 
