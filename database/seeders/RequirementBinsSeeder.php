aaa749e5534aab9d750c31e26230af439e1bf89f
unknown <jrglomar016@gmail.com>
2022-07-23 10:41:40 +0800

    commit for pull

--- database/seeders/RequirementBinsSeeder.php
+++ database/seeders/RequirementBinsSeeder.php
@@ -5,6 +5,11 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\RequirementBin;
 
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 class RequirementBinsSeeder extends Seeder
 {
     /**
@@ -14,6 +19,14 @@ class RequirementBinsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('requirement_bins')->insert([
+            'id' => Str::uuid(),
+            'created_at' => '2022-06-27 18:03:12',
+            'updated_at' => '2022-06-27 18:03:12',
+            'created_by' => 'b1fda120-82ae-49d3-811d-b3c9d5d747a1',
+            'title' => 'Final Requirement [ Project Plan ]',
+            'description' => 'Project planning is the process of d',
+            'deadline' => '2022-06-30 23:59:00'
+        ]);
     }
 }
